5fcc052b17d35aefeedd61f8fa9b42c47ab3eb0a
mrmainly <ernest_everstov@mail.ru>
2022-11-17 22:50:33 +0900

    added basis

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+    return (
+        <div>
+            asdf
+            <Outlet />
+            asd
+        </div>
+    );
+};
+
+export default MainLayout;
